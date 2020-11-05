7e61f2c7b46e177aca4d4f219f75a522ffcf2797
luutry <luutry@163.com>
2020-11-05 11:11:17 +0800

    新建项目

---
+++ src/main/java/com/luutry/onlinemart/OnlineMartApplication.java
@@ -0,0 +1,13 @@
+package com.luutry.onlinemart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMartApplication.class, args);
+    }
+
+}
