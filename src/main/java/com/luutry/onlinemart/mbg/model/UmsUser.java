31df93185fb9d896ed9fcaec7d05b5c14f8ffd4e
luutry <luutry@163.com>
2020-11-06 15:15:47 +0800

    mybatis generator

---
+++ src/main/java/com/luutry/onlinemart/mbg/model/UmsUser.java
@@ -0,0 +1,83 @@
+package com.luutry.onlinemart.mbg.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class UmsUser implements Serializable {
+    /**
+     * 主键ID
+     *
+     * @mbg.generated
+     */
+    private Long id;
+
+    /**
+     * 用户名称
+     *
+     * @mbg.generated
+     */
+    private String username;
+
+    /**
+     * 用户密码
+     *
+     * @mbg.generated
+     */
+    private String password;
+
+    /**
+     * 创建时间
+     *
+     * @mbg.generated
+     */
+    private Date createTime;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", username=").append(username);
+        sb.append(", password=").append(password);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
