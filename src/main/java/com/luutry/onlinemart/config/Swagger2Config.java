38b2639bb3373d37794416b1f91a013717a9f90f
luutry <luutry@163.com>
2020-11-16 15:14:36 +0800

    用户注册接口，重新定义用户表

--- src/main/java/com/luutry/onlinemart/config/Swagger2Config.java
+++ src/main/java/com/luutry/onlinemart/config/Swagger2Config.java
@@ -6,13 +6,14 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @ClassName: Swagger2Config
- * @Description:
+ * @Description: Swagger2API文档的配置
  * @Author: Luutry
  * @Date: 2020/11/6 15:46
  */
@@ -22,7 +23,7 @@ public class Swagger2Config {
     @Bean
     public Docket createRestApi(){
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
+            //    .apiInfo(apiInfo())
                 .select()
                 //为当前包下controller生成API文档
                 .apis(RequestHandlerSelectors.basePackage("com.luutry.onlinemart.controller"))
@@ -31,15 +32,14 @@ public class Swagger2Config {
                 //为有@ApiOperation注解的方法生成API文档
 //                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any())
-                .build();
-    }
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("接口文档演示")
+                        .description("接口文档演示")
+                        .version("9.0")
+                        .contact(new Contact("啊啊啊啊","blog.csdn.net","aaa@gmail.com"))
+                        .license("The Apache License")
+                        .licenseUrl("http://www.baidu.com")
+                        .build());
+    };
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Online Mart API")
-                .description("接口文档演示")
-                .contact("luutry")
-                .version("1.0")
-                .build();
-    }
 }
